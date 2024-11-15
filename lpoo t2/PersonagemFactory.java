a5d27e35d0eed5bb09a05fe0d2bf91a097a5d7c9
Lucas Macedo <lucasedumacedo@gmail.com>
2024-11-14 23:15:52 -0400

    feat: adicionando break no switch e verificação nos dados

--- lpoo t2/PersonagemFactory.java
+++ lpoo t2/PersonagemFactory.java
@@ -10,6 +10,21 @@ public class PersonagemFactory {
         double forca = scanner.nextDouble();
         double destreza = scanner.nextDouble();
 
+        while(atributoIsNotValid(saude)){
+            System.out.println("Informe a saúde do personagem (0 a 10):");
+            saude = scanner.nextDouble();
+        }
+
+        while(atributoIsNotValid(forca)){
+            System.out.println("Informa a forca do personagem (0 a 10):");
+            forca = scanner.nextDouble();
+        }
+
+        while(atributoIsNotValid(destreza)){
+            System.out.println("Informe a destreza do personagem (0 a 10):");
+            destreza = scanner.nextDouble();
+        }
+
         System.out.println("Escolha a arma: ");
         Arma arma = null;
         switch (tipoPersonagem) {
@@ -17,18 +32,24 @@ public class PersonagemFactory {
                 System.out.println("1 para Magia da Transmutação, 2 para Psi-kappa");
                 int armaEscolha = scanner.nextInt();
                 arma = (armaEscolha == 1) ? magiaTransmutacao : psiKappa;
+                break;
             }
             case 2: {
                 System.out.println("1 para Espada, 2 para Lança");
                 int armaEscolha = scanner.nextInt();
                 arma = (armaEscolha == 1) ? espada : lanca;
+                break;
             }
             case 3: {
                 System.out.println("1 para Martelo, 2 para Maça");
                 int armaEscolha = scanner.nextInt();
                 arma = (armaEscolha == 1) ? martelo : maca;
+                break;
             }
-            default: System.out.println("Tipo de personagem inválido.");
+            default: {
+                System.out.println("Tipo de personagem inválido.");
+                break;
+            }
         }
 
         switch (tipoPersonagem) {
@@ -41,5 +62,9 @@ public class PersonagemFactory {
             default: 
                 return null;
         }
+    }
+
+    private static boolean atributoIsNotValid(double attr) {
+        return attr > 10 || attr < 0;
     }
 }
